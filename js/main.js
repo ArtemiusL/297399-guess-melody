bfa75c5c843239db38e6262941f2ddb499456e5e
ArtemiusL <lamber95@icloud.com>
2017-05-28 17:05:03 +0300

    3 задание: отрисовка первого экрана и переключение экранов по клавишам

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//Шаблон экранов
+const template = document.querySelector(`#templates`);
+
+//Список экранов
+const templates =  Array.prototype.filter.call(template.content.querySelectorAll(`.main`), function(child) {
+   return child
+});
+
+//Список экранов в нужном порядке
+const sectionList = [templates[4],templates[0],templates[3],templates[2]]
+
+//Контейнер отрисовки
+const container = document.querySelector(`.main`);
+
+
+
+//Отрисовка первого экрана
+const showFirstSection = () => {
+  container.appendChild(sectionList[0]);
+}
+
+showFirstSection();
+
+let = numberSection = 0;
+
+//Ф-ция отрисовки экранов по номеру
+const drawSection = (event) => {
+  console.log(event.keyCode);
+  if(event.keyCode === 37 && numberSection > 0) {
+    numberSection--;
+    container.replaceChild(sectionList[numberSection],container.firstChild);
+  }
+  if(event.keyCode === 39 && numberSection < sectionList.length - 1 ) {
+    numberSection++;
+    container.replaceChild(sectionList[numberSection],container.firstChild);
+  }
+}
+
+let altPress = false;
+
+//Ловим событbt нажатия alt+(</>)
+document.addEventListener(`keydown`, function(event){
+  if(event.altKey){
+    event.preventDefault();
+    altPress = true;
+  }
+});
+
+document.addEventListener(`keyup`, function(event){
+  if(event.altKey){
+    event.preventDefault();
+    altPress = false;
+  }
+});
+
+document.addEventListener(`keydown`, function(event){
+  if(altPress){
+    drawSection(event);
+  }
+});
+
+

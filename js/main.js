848333c862ce03b4a740668f40d7d5e52aee2770
ArtemiusL <lamber95@icloud.com>
2017-05-29 21:19:10 +0300

    Поправил все ошибки Eslinta

--- js/main.js
+++ js/main.js
@@ -1,60 +1,73 @@
-//Шаблон экранов
+/**
+*Шаблон экранов
+*/
 const template = document.querySelector(`#templates`);
 
-//Список экранов
-const templates =  Array.prototype.filter.call(template.content.querySelectorAll(`.main`), function(child) {
-   return child
+/**
+*Список экранов
+*/
+const templates = Array.prototype.filter.call(template.content.querySelectorAll(`.main`), function (child) {
+  return child;
 });
 
-//Список экранов в нужном порядке
-const sectionList = [templates[4],templates[0],templates[3],templates[2]]
+/*
+*Список экранов в нужном порядке
+*/
+const sectionList = [templates[4], templates[0], templates[3], templates[2]];
 
-//Контейнер отрисовки
+/**
+*Контейнер отрисовки
+*/
 const container = document.querySelector(`.main`);
 
+/**
+*Отрисовка первого экрана
+*/
 
-
-//Отрисовка первого экрана
 const showFirstSection = () => {
   container.appendChild(sectionList[0]);
-}
+};
 
 showFirstSection();
 
-let = numberSection = 0;
+let numberSection = 0;
 
-//Ф-ция отрисовки экранов по номеру
+/**
+*Ф-ция отрисовки экранов по номеру
+*@param {Object} event
+*/
 const drawSection = (event) => {
-  console.log(event.keyCode);
-  if(event.keyCode === 37 && numberSection > 0) {
+  if (event.keyCode === 37 && numberSection > 0) {
     numberSection--;
-    container.replaceChild(sectionList[numberSection],container.firstChild);
+    container.replaceChild(sectionList[numberSection], container.firstChild);
   }
-  if(event.keyCode === 39 && numberSection < sectionList.length - 1 ) {
+  if (event.keyCode === 39 && numberSection < sectionList.length - 1) {
     numberSection++;
-    container.replaceChild(sectionList[numberSection],container.firstChild);
+    container.replaceChild(sectionList[numberSection], container.firstChild);
   }
-}
+};
 
 let altPress = false;
 
-//Ловим событbt нажатия alt+(</>)
-document.addEventListener(`keydown`, function(event){
-  if(event.altKey){
+/**
+*Ловим событbt нажатия alt+(</>)
+*/
+document.addEventListener(`keydown`, function (event) {
+  if (event.altKey) {
     event.preventDefault();
     altPress = true;
   }
 });
 
-document.addEventListener(`keyup`, function(event){
-  if(event.altKey){
+document.addEventListener(`keyup`, function (event) {
+  if (event.altKey) {
     event.preventDefault();
     altPress = false;
   }
 });
 
-document.addEventListener(`keydown`, function(event){
-  if(altPress){
+document.addEventListener(`keydown`, function (event) {
+  if (altPress) {
     drawSection(event);
   }
 });
